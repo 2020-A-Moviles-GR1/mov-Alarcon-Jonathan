c4e906829b22428ccafa9a605b693c6b1c0a19f7
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-07-02 10:13:51 -0500

    hasta ciclo de vida

--- 02-android/app/src/main/java/com/example/android/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/android/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Activity","OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Activity","OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Activity","OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Activity","OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Activity","OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Activity","OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Activity","OnDestroy")
     }
 }
