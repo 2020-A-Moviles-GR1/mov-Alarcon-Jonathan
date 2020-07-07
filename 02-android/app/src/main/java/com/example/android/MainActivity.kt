146e0acf8b80f47aef5411574d204886b821ae89
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-07-06 19:04:07 -0500

    Hasta cambiar de actividad

--- 02-android/app/src/main/java/com/example/android/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/android/MainActivity.kt
@@ -1,44 +1,36 @@
 package com.example.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) //aquí es donde hacemos todas las configuraciones de las aplicaciones
         setContentView(R.layout.activity_main)
         Log.i("Activity","OnCreate")
+        btn_ciclo_vida
+            .setOnClickListener { boton ->
+                //this.irCicloDeVida()
+                irCicloDeVida()
+            }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Activity","OnStart")
+    //funcion para ir a una actividad diferente
+    fun irCicloDeVida(){
+        val intentExplicito = Intent(
+            this,
+            CicloVida::class.java
+        )
+        //this.startActivity(intentExplicito)
+        startActivity(intentExplicito) //metodo dentro de la clase
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i("Activity","OnResume")
-    }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("Activity","OnRestart")
-    }
 
-    override fun onPause() {
-        super.onPause()
-        Log.i("Activity","OnPause")
-    }
 
-    override fun onStop() {
-        super.onStop()
-        Log.i("Activity","OnStop")
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Activity","OnDestroy")
-    }
 }
