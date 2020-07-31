1b33771ec793906d69fc2ecfb2df3ecede81cf7e
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-07-31 01:52:24 -0500

    Revert "Merge branch 'desarrollo'"
    This reverts commit 6ce32092ea3aa70d84dc67f48ef9739a59ac7241.

--- 02-android/app/src/main/java/com/example/android/IntentEnviaParametros.kt
+++ 02-android/app/src/main/java/com/example/android/IntentEnviaParametros.kt
@@ -1,89 +1,12 @@
 package com.example.android
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_intent_envia_parametros.*
 
 class IntentEnviaParametros : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_envia_parametros)
-
-
-        //Clase 20/07/2020
-        //Aquí vamos a recibir los parámetros
-                //hay una clase llamada Intent
-        //toca mandarle también un valor por defecto
-        val numeroEncontrado = intent.getIntExtra("numero", 0)// el intent existe dentro de la clase
-        val numeroEncontrado1 = intent.getIntExtra("numero1", 0)
-        val numeroEncontrado2 = intent.getIntExtra("numero2", 0)
-        val numeroEncontrado3 = intent.getIntExtra("numero3", 0)
-        val numeroEncontrado4 = intent.getIntExtra("numero4", 0)
-        val numeroEncontrado5 = intent.getIntExtra("numero5", 0)
-
-        if (numeroEncontrado !=0 ){
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado}")
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado1}")
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado2}")
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado3}")
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado4}")
-            Log.i("intents","El numero encontrado es: ${numeroEncontrado5}")
-        }
-
-        //Tenemos que poner una validación por que puede ser nulo
-        val textoCompartido: String? = intent.getStringExtra(Intent.EXTRA_TEXT)
-        if(textoCompartido != null){
-            Log.i("intents","el texto es ${textoCompartido}")
-        }
-
-
-        //¿Como volver a la pantalla anterior
-
-        btn_devolver_respuesta.setOnClickListener {
-            //metodo de la clase
-            //this.finish()
-            finish() // con esta parte se regresa a la pantalla
-        }
-
-        btn_resp_aceptar.setOnClickListener {
-            val nombre = "Jonathan"
-            val edad = 24
-            val intentRespuesta = Intent()
-            intentRespuesta.putExtra("nombre",nombre)
-            intentRespuesta.putExtra("edad",edad)
-            setResult(
-                Activity.RESULT_OK,
-                intentRespuesta
-            )
-            finish()
-        }
-
-        btn_resp_cancelar.setOnClickListener {
-            val intentCancelado = Intent()
-            setResult(
-                Activity.RESULT_CANCELED,
-                intentCancelado
-            )
-            finish()
-        }
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
-
-
 }
--- 02-android/app/src/main/java/com/example/android/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/android/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.android
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_intent_envia_parametros.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,90 +22,7 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener { boton ->
                 irListView()
             }
-
-
-        btn_intent_respuesta.setOnClickListener {
-            irAIntentConRespuesta()
-        }
-
-        btn_inten_implicito.setOnClickListener {
-            enviarIntentConRespuesta()
-        }
-
-        btn_resp_propia.setOnClickListener {
-            enviarIntenConRespuestaPropia()
-        }
-
     }
-
-
-    fun enviarIntenConRespuestaPropia(){
-        val intentConRespuesta = Intent(
-            this,
-            IntentEnviaParametros ::class.java
-        )
-        //this.startActivityForResult(intentConRespuesta,304)
-        startActivityForResult(intentConRespuesta,305)
-
-    }
-
-    fun enviarIntentConRespuesta(){
-        val intentConRespuesta = Intent(
-            Intent.ACTION_PICK,
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-        )
-        //this.startActivityForResult(intentConRespuesta,304)
-        startActivityForResult(intentConRespuesta,304)
-
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ){
-        super.onActivityResult(requestCode,resultCode, data)
-        when(resultCode){
-            Activity.RESULT_OK ->{
-                Log.i("Resultado", "Ok")
-                when(requestCode){
-                    305 -> {
-                        if(data != null){
-                            val nombre = data.getStringExtra("nombre")
-                            val edad = data.getIntExtra("edad",0)
-                            Log.i("R esultado", "Nombre: ${nombre} Edad: ${edad}")
-                        }
-                    }
-                }
-
-            }
-            Activity.RESULT_CANCELED -> {
-                Log.i("Resultado", ":(")
-            }
-        }
-
-        /*val uri = data?.data
-        if (uri != null) {
-            val cursor = contentResolver.query(
-                uri,
-                null,
-                null,
-                null,
-                null,
-                null
-            )
-            cursor?.moveToFirst()
-            val indiceTelefono = cursor?.getColumnIndex(
-                ContactsContract.CommonDataKinds.Phone.NUMBER
-            )
-            val telefono = cursor?.getString(indiceTelefono!!)
-            cursor?.close()
-            Log.i("resultado", "Telefono: ${telefono}")
-        }*/
-
-
-    }
-
 
     //funcion para ir a una actividad diferente
     fun irCicloDeVida(){
@@ -128,54 +42,6 @@ class MainActivity : AppCompatActivity() {
         //this.startActivity(intentExplicito)
         startActivity(intentExplicito) //metodo dentro de la clase
     }
-
-
-    //Clase 20/07/2020
-    fun irAIntentConRespuesta(){
-        //parte del envío de respuesta
-        val intentExplicito = Intent(
-            this,
-            IntentEnviaParametros :: class.java
-        )
-        intentExplicito.putExtra("numero", 2) // De esta manera enviamos valores primitivos
-        intentExplicito.putExtra("numero1", 3)
-        intentExplicito.putExtra("numero2", 4)
-        intentExplicito.putExtra("numero3", 5)
-        intentExplicito.putExtra("numero4", 6)
-        intentExplicito.putExtra("numero5", 7)
-        startActivity(intentExplicito)
-
-    }
-
-
-
-
-    /*override fun OnActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ){
-
-    }*/
-
-    /*override fun OnActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ){
-        super.onActivityResult(requestCode,resultCode, data)
-        when(resultCode){
-            Activity.RESULT_OK ->{
-                Log.i("Resultado", "Ok")
-            }
-            Activity.RESULT_CANCELED -> {
-                Log.i("Resultado", ":(")
-            }
-        }
-    }*/
-
-
-
 
 
 
