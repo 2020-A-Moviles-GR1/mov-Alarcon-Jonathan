78819fbf1ec0792749b9314ff034168820e546aa
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-09-14 08:54:57 -0500

    Hasta listeners

--- 02-android/app/src/main/java/com/example/android/MapsActivity.kt
+++ 02-android/app/src/main/java/com/example/android/MapsActivity.kt
@@ -12,10 +12,46 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(),
+    OnMapReadyCallback,
+    GoogleMap.OnCameraMoveStartedListener,
+    GoogleMap.OnCameraMoveListener,
+    GoogleMap.OnCameraIdleListener,
+    GoogleMap.OnPolylineClickListener,
+    GoogleMap.OnPolygonClickListener {
+
+    // Funcion para los listeners
+    fun establecerListeners(map: GoogleMap){
+        with(map){
+            setOnCameraIdleListener(this@MapsActivity)
+            setOnCameraMoveStartedListener(this@MapsActivity)
+            setOnCameraMoveListener(this@MapsActivity)
+            setOnPolylineClickListener(this@MapsActivity)
+            setOnPolygonClickListener(this@MapsActivity)
+        }
+    }
+
+    override fun onCameraMoveStarted(p0: Int) {
+        Log.i("mapa", "Empezando a mover onCameraMoveStarted")
+    }
+
+    override fun onCameraMove() {
+        Log.i("mapa", "Moviendo onCameraMove")
+    }
+
+    override fun onCameraIdle() {
+        Log.i("mapa", "Quieto onCameraIdle")
+    }
+
+    override fun onPolylineClick(p0: Polyline?) {
+        Log.i("mapa", "Polylinea ${p0.toString()}")
+    }
+
+    override fun onPolygonClick(p0: Polygon?) {
+        Log.i("mapa", "Poligono ${p0.toString()}")
+    }
 
     private lateinit var mMap: GoogleMap
 
@@ -39,6 +75,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // 2) Establecer configuraciones
         establecerConfiguraciones(mMap)
 
+        establecerListeners(mMap)
+
         val direccion = LatLng(-0.208085, -78.507360)
         val puntoUsuario = LatLng(-0.209812, -78.506255)
         val zoom = 17f
@@ -46,8 +84,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         anadirMarcador(direccion, titulo)
         moverCamaraConZoom(puntoUsuario, zoom)
 
+        val poliLineasUno = googleMap.addPolyline(
+            PolylineOptions()
+                .clickable(true)
+                .add(
+                    LatLng(-0.207449, -78.505400),
+                    LatLng(-0.207664, -78.504735),
+                    LatLng(-0.208447, -78.504896),
+                    LatLng(-0.208147, -78.505813)
+                )
+        )
+
+        val poligonoUno = googleMap.addPolygon(
+            PolygonOptions()
+                .clickable(true)
+                .add(
+                    LatLng(-0.207063, -78.507031),
+                    LatLng(-0.207176, -78.506248),
+                    LatLng(-0.207938, -78.506463),
+                    LatLng(-0.207675, -78.507230)
+                )
+        )
+        poligonoUno.fillColor = 0xc771c4
+
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        //val sydney = LatLng(-34.0, 151.0)
         //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
